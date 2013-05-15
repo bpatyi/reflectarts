6e05ddfd8ff2ef53b4ac93229d5401a20fb04a53
Kilgharrah <19noname19@gmail.com>
2013-05-15 18:16:53 +0200

    create intros app

---
+++ intros/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from subpages.models import Subpage
+
+class Intro(models.Model):
+    subpage = models.ForeignKey('Subpage', unique=True)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
+
+    def __unicode__(self):
+        return self.content
