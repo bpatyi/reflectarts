0ba4120d4863d0a6ba4074136af92f69421245b2
Kilgharrah <19noname19@gmail.com>
2013-05-15 17:38:42 +0200

    create newsletter app

---
+++ newsletter/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class NewsLetter(models.Model):
+    title = models.CharField(max_length=80, verbose_name=u'title')
+    url = models.URLField(max_length=200, verbose_name=u'url')
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
+
+    def __unicode__(self):
+        return u'%s' % self.title
