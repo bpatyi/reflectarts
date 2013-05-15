d2434bcadd4e428e41e84e268f1ac31445c8394c
Kilgharrah <19noname19@gmail.com>
2013-05-15 17:38:56 +0200

    create pictures app

---
+++ pictures/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
+
+from exhibitions.models import Exhibition
+
+class Picture(models.Model):
+    title = models.CharField(max_length=128, verbose_name=u'title',)
+    description = models.TextField(verbose_name=u'description',)
+    exhibition = models.ForeignKey('Exhibition', verbose_name=u'exhibition')
+
+    image = ThumbnailerImageField(upload_to='pictures', blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
+
+    def __unicode__(self):
+        return u'%s' % self.title
