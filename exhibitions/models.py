83dc96f3ff628c97c137e6cb32a8949b1c9528a8
Kilgharrah <19noname19@gmail.com>
2013-05-15 17:38:06 +0200

    create exhibition model

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Exhibition(models.Model):
+    title = models.CharField(max_length=128, verbose_name=u'title',)
+    description = models.TextField(verbose_name=u'description',)
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u"crated at")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u"updated at")
+
+    def __unicode__(self):
+        return u'%s' % self.title
