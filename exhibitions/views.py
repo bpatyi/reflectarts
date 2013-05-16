aad012a90803fcaf091ba62e8dc3def8505ee8b4
Kilgharrah <19noname19@gmail.com>
2013-05-16 12:16:23 +0200

    create exhibitionlist view

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.views.generic import View, ListView
+from django.template.response import TemplateResponse, HttpResponse
+from exhibition.models import Exhibition, ExhibitionDesciptor
+
+
+class ExhibitionList(ListView):
+    context_object_name = "exhibition_list"
+    template_name = "exhibition_list.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Exhibiton.objects.all().order_by('-exhibitiondescriptor__from_date')
+
+    def get_context_data(self, **kwargs):
+        context = super(ExhibitionList, self).get_context_data(**kwargs)
+        return context
