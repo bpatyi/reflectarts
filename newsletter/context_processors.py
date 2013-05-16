98950cac2939b0b3fe4e8c9ef5134e3c2fda1750
Kilgharrah <19noname19@gmail.com>
2013-05-16 17:39:05 +0200

    fix newsletter processor

--- newsletter/context_processors.py
+++ newsletter/context_processors.py
@@ -1,12 +1,5 @@
 from newsletter.models import NewsLetter
 
 def newsletter_processor(request):
-    try:
-        newsletter_ = NewsLetter.objects.all().order_by('date')
-    except NewsLetter.DoesNotExist:
-        newsletter_ = None
-
-    if len(newsletter_) > 10:
-        return { 'newsletter': newsletter_[:10] }
-    else:
-        return { 'newsletter': newsletter_ }
+    newsletter = NewsLetter.objects.all().order_by('-date')
+    return { 'newsletter': newsletter[:10] }
