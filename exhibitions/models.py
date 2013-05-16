dafe4d9a83e7dd6bd7ecf21aa3ccd307088674f9
Kilgharrah <19noname19@gmail.com>
2013-05-16 12:42:15 +0200

    WIP exhibition model

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -9,20 +9,14 @@ class Exhibition(models.Model):
 
     slug = AutoSlugField(populate_from='title', slugify=lambda value: value.replace(' ','-'))
 
-    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u"crated at")
-    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u"updated at")
-
-    def __unicode__(self):
-        return u'%s' % self.title
-
-
-class ExhibitionDesciptor(models.Model):
-    exhibition = models.ForeignKey('Exhibition')
     place = models.CharField(max_length=255)
     extra_description = models.TextField()
 
     from_date = models.DateField()
     to_date = models.DateField()
 
-    def __unicode__(self)
-        return '%s' % (self.exhibition.title)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u"crated at")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u"updated at")
+
+    def __unicode__(self):
+        return u'%s' % self.title
