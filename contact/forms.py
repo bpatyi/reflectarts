70cd7b73cd9d76a15247630b4ce5a8e4b68563a6
Kilgharrah <19noname19@gmail.com>
2013-05-16 13:47:20 +0200

    fixex in contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class ContactForm(forms.Form)
+class ContactForm(forms.Form):
     name = forms.CharField(max_length=50,)
     email = forms.EmailField()
     comment = forms.CharField(widget=forms.Textarea,)
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import View
 from django.template.response import TemplateResponse, HttpResponse
 
-form contact.models import Contact
+from contact.models import Contact
 from contact.forms import ContactForm
 
 class Contact(View):
@@ -10,7 +10,7 @@ class Contact(View):
     form = ContactForm
 
     def get(self, request, *args, **kwargs):
-        contact_information = Contact.objects.all()[0]
+        contact_information = None
 
         form = self.form()
 
