5f7aaca162fdf8dfaf8feade2cf86a5fbd0c8429
Kilgharrah <19noname19@gmail.com>
2013-05-16 14:19:34 +0200

    fix intro admin

--- intros/admin.py
+++ intros/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from intros.models import Intro
 
 class IntroAdmin(admin.ModelAdmin):
-    list_display = ('subpage__title', 'created_at', 'updated_at')
+    list_display = ('get_subpage_title', 'created_at', 'updated_at')
+
+    def get_subpage_title(self, obj):
+        return '%s' % (obj.subpage.title)
 
 admin.site.register(Intro, IntroAdmin)
