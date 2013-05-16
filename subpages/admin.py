3ecd4d06f0d6b2f2b341c6289755833c0f7af784
Kilgharrah <19noname19@gmail.com>
2013-05-16 16:34:54 +0200

    create introinline admin to subpage admin

--- subpages/admin.py
+++ subpages/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from subpages.models import Subpage
+from intros.models import Intro
 from django.conf import settings
 
+
+class IntroInline(admin.TabularInline):
+    model = Intro
+
+
 class SubpageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
+    list_display = ('pk', 'name', 'created_at', 'updated_at')
+    inlines = [
+        IntroInline,
+    ]
 
     class Media:
         js = (settings.STATIC_URL + 'js/tiny_mce/tiny_mce.js',
