b1148c1e463e1d0cf67096fac6165ef8af3dd17c
Kilgharrah <19noname19@gmail.com>
2013-05-16 14:20:05 +0200

    fix pictures admin

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from pictures.models import Picture
 
-class PictureAdmin(admin.ModelAdmin)
-    list_display = ('title', 'exhibition__title', 'crated_at', 'updated_at')
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_exhibition_title', 'created_at', 'updated_at')
+
+    def get_exhibition_title(self, obj):
+        return '%s' % (obj.exhibition.title)
+
+admin.site.register(Picture, PictureAdmin)
