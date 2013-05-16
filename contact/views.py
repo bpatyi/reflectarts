53b399e432df7c3c20df2d3b796ca815a9564828
Kilgharrah <19noname19@gmail.com>
2013-05-16 13:06:50 +0200

    wip contact view

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,23 @@
 from django.views.generic import View
 from django.template.response import TemplateResponse, HttpResponse
 
+form contact.models import Contact
+from contact.forms import ContactForm
 
 class Contact(View):
     template = 'contact.html'
     http_method_names = ['get', 'post']
+    form = ContactForm
 
     def get(self, request, *args, **kwargs):
-        context = {}
+        contact_information = Contact.objects.all()[0]
+
+        form = self.form()
+
+        context = {
+            'contact_information': contact_information,
+            'contact_form': form,
+        }
 
         return TemplateResponse(request, self.template, context)
 
