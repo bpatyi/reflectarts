06231f76a230f41df9bb05e7f6603cee89313cc0
Kilgharrah <19noname19@gmail.com>
2013-05-16 10:29:29 +0200

    create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
+
+    def __unicode__(self):
+        return self.name
