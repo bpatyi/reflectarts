36361ddeecbc32bec797dc711401a924cdb7c13c
Kilgharrah <19noname19@gmail.com>
2013-05-16 11:08:15 +0200

    create contact view

--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.views.generic import View
+from django.template.response import TemplateResponse, HttpResponse
+
+
+class Contact(View):
+    template = 'contact.html'
+    http_method_names = ['get', 'post']
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+
+        return TemplateResponse(request, self.template, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+
+        return TemplateResponse(request, self.template, context)
