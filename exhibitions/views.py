e69c124ab30031216d88be1a3dfef0fd8f9da771
Kilgharrah <19noname19@gmail.com>
2013-05-16 12:42:48 +0200

    wip exhibition list, detail view

--- exhibitions/views.py
+++ exhibitions/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import View, ListView
+from django.views.generic import View, ListView, DetailView
 from django.template.response import TemplateResponse, HttpResponse
+
 from exhibition.models import Exhibition, ExhibitionDesciptor
 
 
@@ -13,4 +14,26 @@ class ExhibitionList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(ExhibitionList, self).get_context_data(**kwargs)
+        return context
+
+class ExhibitionDetail(DetailView):
+    model = Exhibition
+    template_name = 'exhibition_detail.html'
+    queryset = Exhibition.objects.all()
+
+    def __init__(self, *args, **kwargs):
+        super(ArtistDetail, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        qs = super(ArtistDetail, self).get_queryset()
+        slug = self.kwargs.get('slug', None)
+
+        qs = Exhibition.object.get(slug=slug)
+
+        return qs
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ApplicationDetail, self).get_context_data(**kwargs)
+
         return context
