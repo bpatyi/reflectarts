2ab07e42533829a0307fd3ba7a5fddd6c9ef579d
Kilgharrah <19noname19@gmail.com>
2013-05-16 17:38:09 +0200

    contact get intro

--- contact/views.py
+++ contact/views.py
@@ -3,11 +3,14 @@ from django.template.response import TemplateResponse, HttpResponse
 
 from contact.models import Contact
 from contact.forms import ContactForm
+from utils.tools import getIntro
+
 
 class Contact(View):
     template = 'contact.html'
     http_method_names = ['get', 'post']
     form = ContactForm
+    intro = getIntro(5)
 
     def get(self, request, *args, **kwargs):
         contact_information = None
@@ -17,6 +20,7 @@ class Contact(View):
         context = {
             'contact_information': contact_information,
             'contact_form': form,
+            'intro': self.intro,
         }
 
         return TemplateResponse(request, self.template, context)
