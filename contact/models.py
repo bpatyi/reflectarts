f1a4573e909726cdef3b99a2ad52158fbe86ed66
Kilgharrah <19noname19@gmail.com>
2013-05-17 10:18:52 +0200

    FIXES in contact models

--- contact/models.py
+++ contact/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Contact(models.Model):
-    name = models.CharField(max_length=128, blank=True, null=True)
+    name = models.CharField(max_length=128, blank=True, null=True, default='')
     email = models.EmailField()
-    description = models.TextField(blank=True, null=True)
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(blank=True, null=True, default='')
+    phone = models.CharField(max_length=20, default='',)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
     updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
