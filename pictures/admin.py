538585ef0e20c83a5713fa5d12224a0d0e95537a
Kilgharrah <19noname19@gmail.com>
2013-05-17 12:11:09 +0200

    create thumbnail on pictures list

--- pictures/admin.py
+++ pictures/admin.py
@@ -3,10 +3,21 @@ from pictures.models import Picture
 from django.conf import settings
 
 class PictureAdmin(admin.ModelAdmin):
-    list_display = ('title', 'get_exhibition_title', 'created_at', 'updated_at')
+    list_display = ('title', 'image_thumbnail', 'get_exhibition_title', 'created_at', 'updated_at')
 
     def get_exhibition_title(self, obj):
         return '%s' % (obj.exhibition.title)
+
+    def image_thumbnail(self, obj):
+        thumb_url = get_thumbnailer(obj.image)['avatar'].url
+
+        if obj.image and thumb_url:
+            return '<img src="%s" />' % thumb_url
+        else:
+            return ""
+
+    image_thumbnail.allow_tags = True
+    image_thumbnail.short_description = "Thumbnail"
 
     class Media:
         js = (
