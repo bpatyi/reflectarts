417b76b012be767c3ea85198ee35af00f7c70100
Kilgharrah <19noname19@gmail.com>
2013-05-21 12:51:20 +0200

    FIX contact model

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=128, blank=True, null=True, default='')
     email = models.EmailField()
     description = models.TextField(blank=True, null=True, default='')
-    phone = models.CharField(max_length=20, default='',)
+    phone = models.CharField(max_length=20, default='', blank=True, null=True,)
 
     created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=u'crated at')
     updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name=u'updated at')
