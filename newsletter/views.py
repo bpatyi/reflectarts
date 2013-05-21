44885838b48bba8a4e2735947278d1b591eebc95
Kilgharrah <19noname19@gmail.com>
2013-05-21 11:15:45 +0200

    create NewsLetterList View

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.views.generic import View, ListView, DetailView
+from django.template.response import TemplateResponse, HttpResponse
+
+from newsletter.models import NewsLetter
+
+class NewsLetterList(ListView):
+    context_object_name = "newsletter_list"
+    template_name = "newsletter_list.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return NewsLetter.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(NewsLetterList, self).get_context_data(**kwargs)
+        return context
