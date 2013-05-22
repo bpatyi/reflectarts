f8f1f92446628dba53e49c71ac51e3193825faa8
Kilgharrah <19noname19@gmail.com>
2013-05-22 12:47:04 +0200

    WIP artists model ( selected field)

--- artists/models.py
+++ artists/models.py
@@ -5,12 +5,14 @@ from django.conf import settings
 
 
 class Artist(models.Model):
-    first_name = models.CharField(max_length=128, verbose_name=u'first name', unique=True,)
-    last_name = models.CharField(max_length=128, verbose_name=u'last name',)
+    first_name = models.CharField(max_length=128, verbose_name=u'first name', help_text="First name of artist")
+    last_name = models.CharField(max_length=128, verbose_name=u'last name', help_text="Last name of artist")
     email = models.EmailField(default=settings.REFLECT_NOTIFY_EMAIL)
     description = models.TextField(verbose_name=u'description', blank=True, null=True)
     slug = AutoSlugField(populate_from='get_name', slugify=lambda value: value.replace(' ','-'))
     website_url = models.URLField(null=True, blank=True,)
+
+    selected = models.BooleanField(default=False, verbose_name="Represented", help_text="Whether to display on index page (represented)")
 
     image = ThumbnailerImageField(upload_to='artists', blank=True)
 
